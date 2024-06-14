0ba26bc8c084b151aa5b2fb23ae5ed2e764f37ad
Budubuu <162087289+Budubuu@users.noreply.github.com>
2024-06-14 11:38:25 +0900

    env.constant

--- src/constants/env.constant.js
+++ src/constants/env.constant.js
@@ -1,4 +1,7 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const SERVER_PORT = process.env.SERVER_PORT;
+export const DATABASE_URL = process.env.DATABASE_URL;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
